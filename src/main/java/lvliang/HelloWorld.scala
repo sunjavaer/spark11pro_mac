4d66340337966270ff25b1a5957331450e4b2511
lvliang <lvliang0403@163.com>
2019-05-21 09:53:40 +0800

    Hello World

---
+++ src/main/java/lvliang/HelloWorld.scala
@@ -0,0 +1,16 @@
+package lvliang
+
+/**
+  * <p>Company:misspao </p >
+  *
+  * @author: lvliang
+  * @Date: Create in 09:50 2019-05-21
+  * @Description:
+  */
+object HelloWorld {
+
+  def main(args: Array[String]): Unit = {
+    print("hello world")
+  }
+
+}
