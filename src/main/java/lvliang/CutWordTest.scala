98130a30cb735728c13b8fcdb8c021eaf1984bf3
lvliang <lvliang0403@163.com>
2019-05-23 18:34:53 +0800

    UDF Test

--- src/main/java/lvliang/CutWordTest.scala
+++ src/main/java/lvliang/CutWordTest.scala
@@ -1,5 +1,7 @@
 package lvliang
 
+import org.apache.spark.sql.SparkSession
+
 /**
   * <p>Company:misspao </p >
   *
@@ -11,6 +13,20 @@ object CutWordTest {
 
   def main(args: Array[String]): Unit = {
     print("cut word test")
-  }
 
+    var warehouseLocation = "/usr/hive/warehouse"
+    val spark: SparkSession = SparkSession.builder
+      .appName("SparkTest")
+      //      .master("local[*]")               //提交模式交给spark-submit控制
+      .config("spark.sql.warehouse.dir", warehouseLocation)
+      .enableHiveSupport()
+      .getOrCreate
+
+    val orders = spark.sql("select * from misspao.mp_deposit_order")
+
+    import org.apache.spark.sql.functions._
+    orders.filter(col("description").notEqual("")).show(5)
+
+    spark.stop()
+  }
 }
