88e34725b14c39e0a12c4f1da86987af898570ea
lvliang <lvliang0403@163.com>
2019-05-24 20:04:55 +0800

    word 转换为scala类

---
+++ src/main/java/lvliang/Test.scala
@@ -0,0 +1,16 @@
+package lvliang
+
+/**
+  * <p>Company:misspao </p >
+  *
+  * @author: lvliang
+  * @Date: Create in 17:44 2019-05-24
+  * @Description:
+  */
+class Test {
+
+  def say(s: String) = {
+    println(s)
+  }
+
+}
