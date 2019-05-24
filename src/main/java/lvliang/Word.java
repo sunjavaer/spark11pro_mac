7b701b6358e4cbdf6512943df2276e82b7510aea
lvliang <lvliang0403@163.com>
2019-05-24 17:23:28 +0800

    word 增加序列化接口

--- src/main/java/lvliang/Word.java
+++ src/main/java/lvliang/Word.java
@@ -1,5 +1,7 @@
 package lvliang;
 
+import java.io.Serializable;
+
 /**
  * <p>Company:misspao </p >
  *
@@ -7,7 +9,7 @@ package lvliang;
  * @Date: Create in 10:40 2019-05-24
  * @Description:
  */
-public class Word {
+public class Word implements Serializable {
 
     public void say(String s) {
         System.out.println("------" + s);
